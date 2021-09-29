db7ef7083e6deef6bd3f6c523a2fe3e0d9114b5f
unknown <jchernandez827@gmail.com>
2021-09-29 09:14:05 -0500

    Create index for foreign keys

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -46,3 +46,10 @@ ALTER TABLE medical_histories ADD CONSTRAINT medical_histories_fk FOREIGN KEY (i
 ALTER TABLE invoices ADD CONSTRAINT invoices_fk FOREIGN KEY (id) REFERENCES medical_histories(id) ON DELETE RESTRICT ON UPDATE CASCADE;
 ALTER TABLE invoice_items ADD CONSTRAINT invoice_items_fk FOREIGN KEY (id) REFERENCES invoices(id) ON DELETE RESTRICT ON UPDATE CASCADE;
 ALTER TABLE invoice_items ADD CONSTRAINT invoice_items_fk_1 FOREIGN KEY (id) REFERENCES treatments(id) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+CREATE INDEX my_patients_id ON medical_histories (patient_id ASC);
+CREATE INDEX my_invoices_id ON invoices (medical_history_id ASC);
+CREATE INDEX my_invoice_items_id ON invoice_items (invoice_id ASC);
+CREATE INDEX my_invoice_treatment_id ON invoice_items (treatment_id ASC);
+CREATE INDEX my_medical_histories_id ON histories_treatments (medical_histories_id ASC);
+CREATE INDEX my_treatments_id ON histories_treatments (treatments_id ASC);
